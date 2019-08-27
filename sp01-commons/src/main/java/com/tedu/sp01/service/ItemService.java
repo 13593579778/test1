7ebb72413abe474a061e7b065e273953ebfd31b7
Administrator <Administrator@7PUBXIFQJ0BCVY4>
2019-08-27 19:58:02 +0800

    sp01

---
+++ sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
@@ -0,0 +1,11 @@
+package com.tedu.sp01.service;
+
+import java.util.List;
+
+import com.tedu.sp01.pojo.Item;
+
+public interface ItemService {
+List<Item> getItem(String orderId);
+void decreaseNumbers(List<Item> list);
+
+}
